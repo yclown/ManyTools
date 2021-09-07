9171f0faa4deffcb254cb849d1795d15935142d5
yclown <yclown@163.com>
2021-09-07 14:42:01 +0800

    基础搭建
    基础搭建

---
+++ content/js/Config.js
@@ -0,0 +1,23 @@
+
+// {
+//     name:"页面名称", 
+//     url:"链接",
+//     desc:"页面描述",
+//     img:"页面icon"
+// },
+var pages=[
+    {
+        Cag:"饥荒",
+        Page:[
+            {
+                name:"饥荒服务器mod提取工具",
+                url:"html/dstmodtools.html",
+                desc:"饥荒服务器mod提取工具",
+                img:""
+            },
+        ]
+
+    },
+     
+]
+
